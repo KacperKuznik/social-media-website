842677f50d60546e258684ae4c60a25eb9b73bcc
kacperkuznik <kuznikkacper@gmail.com>
2022-05-11 21:00:37 +0200

    notifications

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -6,10 +6,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-    def get_photo_url(self, obj):
-        request = self.context.get('request')
-        photo_url = obj.fingerprint.url
-        return request.build_absolute_uri(photo_url)
 
 
 class NotificationSerializer(serializers.ModelSerializer):
--- frontend/src/components/Notifications.js
+++ frontend/src/components/Notifications.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import notifications_icon from '../img/notifications.png'
 import './Notifications.css'
-
+import useCollapse from 'react-collapsed';
 
 function Notifications(props) {
+    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
     const [notifications, setNotifications] = useState([])
     useEffect(() => {
       axios.get('/users/notifications/').then((res) => setNotifications(res.data))
@@ -12,10 +13,12 @@ function Notifications(props) {
     }, [])
     
     return (
-        <div className="notifications">
-            <img src={notifications_icon}></img>
+        <div className="notifications" >
+            <img src={notifications_icon} {...getToggleProps()}></img>
             {notifications.length > 0 ? <div className="notifications-count">{notifications.length}</div> : null}
-            {notifications?.map(notification => console.log(notification))}
+            <ul {...getCollapseProps()}>
+                {notifications?.map(notification => <li className="content" key={notification.id} >{notification.message}</li>)}     
+            </ul>
         </div>
     );
 }
