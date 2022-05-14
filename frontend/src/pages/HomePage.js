4adc02b5f3e81137c136f3247f1ea03ba4a27f43
kacperkuznik <kuznikkacper@gmail.com>
2022-05-14 12:02:54 +0200

    home posts

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,13 +1,23 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Navbar from "../components/navbar/Navbar"
+import PostList from '../components/posts/PostList'
 
 function HomePage() {
-
+  const [homePosts, setHomePosts] = useState([])
+  useEffect(() => {
+    axios.get('/posts/home').then((res) =>{
+      setHomePosts(res.data);
+      console.table(res.data)
+    })
+  }, [])
+  
 
 
   return (
     <div>
       <Navbar />
-
+      <PostList posts={homePosts}/>
     </div>
   );
 }
